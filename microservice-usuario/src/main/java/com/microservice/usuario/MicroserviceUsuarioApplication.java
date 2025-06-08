ffe679a619ba54ce000ce6bc6413d0b9561fc30e
elDone <diego-o.o-@hotmail.com>
2025-06-07 20:09:22 -0500

    feat: crea mcsv user

---
+++ microservice-usuario/src/main/java/com/microservice/usuario/MicroserviceUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceUsuarioApplication.class, args);
+	}
+
+}
