760da9a3e6611e0859a85c992b7bf52eba420bd5
elDone <diego-o.o-@hotmail.com>
2025-06-08 19:26:35 -0500

    feat: crea mcsv especialidad

---
+++ microservice-especialidad/src/main/java/com/example/especialidad/MicroserviceEspecialidadApplication.java
@@ -0,0 +1,13 @@
+package com.example.especialidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEspecialidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceEspecialidadApplication.class, args);
+	}
+
+}
---
+++ microservice-especialidad/src/test/java/com/example/especialidad/MicroserviceEspecialidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.especialidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEspecialidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
