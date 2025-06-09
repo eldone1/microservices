c68d853ff36c432528c3b44f556b837d571de4b8
elDone <diego-o.o-@hotmail.com>
2025-06-08 19:32:35 -0500

    enable discovery client

--- microservice-especialidad/src/main/java/com/example/especialidad/MicroserviceEspecialidadApplication.java
+++ microservice-especialidad/src/main/java/com/example/especialidad/MicroserviceEspecialidadApplication.java
@@ -3,6 +3,7 @@ package com.example.especialidad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceEspecialidadApplication {
 
