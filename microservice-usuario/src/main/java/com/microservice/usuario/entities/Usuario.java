733086c167e0907f8489062b2fe0f8a19f572d98
elDone <diego-o.o-@hotmail.com>
2025-06-09 23:22:01 -0500

    changes usuario entity

---
+++ microservice-usuario/src/main/java/com/microservice/usuario/entities/Usuario.java
@@ -0,0 +1,47 @@
+package com.microservice.usuario.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@Table(name = "usuario")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+
+    private String apellidos;
+    private String email;
+    private String telefono;
+
+    @Column(name = "fecha_registro")
+    private LocalDateTime fechaRegistro;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password; 
+
+    @Enumerated(EnumType.STRING)
+    private RolUsuario rol;
+
+}
