0c4c21c3549f6899fb9e58386b638384441c580e
elDone <diego-o.o-@hotmail.com>
2025-06-11 19:16:41 -0500

    Add metodos especificos repository

--- microservice-usuario/src/main/java/com/microservice/usuario/persistence/UsuarioRepository.java
+++ microservice-usuario/src/main/java/com/microservice/usuario/persistence/UsuarioRepository.java
@@ -1,15 +1,20 @@
 package com.microservice.usuario.persistence;
 
+import java.lang.StackWalker.Option;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.microservice.usuario.entities.RolUsuario;
 import com.microservice.usuario.entities.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
-    List<Usuario> findByRol(String rol);
+    List<Usuario> findByRol(RolUsuario rol);
+
+    Optional<Usuario> findByUsername(String username);
 
 }
