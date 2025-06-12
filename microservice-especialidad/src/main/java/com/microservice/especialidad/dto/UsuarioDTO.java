cd60534e40485c3d9a1a38a625725cfb7da6ac2d
elDone <diego-o.o-@hotmail.com>
2025-06-12 17:07:52 -0500

    add dto user

---
+++ microservice-especialidad/src/main/java/com/microservice/especialidad/dto/UsuarioDTO.java
@@ -0,0 +1,22 @@
+package com.microservice.especialidad.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioDTO {
+
+    private String nombre;
+
+    private String apellidos;
+    private String email;
+    private String telefono;
+
+  
+    private Long especialidadId;
+}
