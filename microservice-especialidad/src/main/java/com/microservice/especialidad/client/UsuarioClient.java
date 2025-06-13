668b8c5d497b3a7e0932db4cfe572d2a47e5ebab
elDone <diego-o.o-@hotmail.com>
2025-06-12 22:41:15 -0500

    add package client

--- microservice-especialidad/src/main/java/com/microservice/especialidad/client/UsuarioClient.java
+++ microservice-especialidad/src/main/java/com/microservice/especialidad/client/UsuarioClient.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "mcsv-usuario", url = "localhost:8090/api/usuario")
 public interface UsuarioClient {
 
     @GetMapping("/search-by-especialidad/{especialidadId}")
-    List<?> findAllUsuariosByEspecialidad();
+    List<?> findAllUsuariosByEspecialidad(@PathVariable Long especialidadId);
 }
