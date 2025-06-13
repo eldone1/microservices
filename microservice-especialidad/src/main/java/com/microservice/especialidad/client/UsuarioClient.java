a06f66899ac3d41a078c792e459b61a9b43dbf0a
elDone <diego-o.o-@hotmail.com>
2025-06-13 09:18:38 -0500

    modif to list UsuarioDTO

--- microservice-especialidad/src/main/java/com/microservice/especialidad/client/UsuarioClient.java
+++ microservice-especialidad/src/main/java/com/microservice/especialidad/client/UsuarioClient.java
@@ -6,9 +6,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.microservice.especialidad.dto.UsuarioDTO;
+
 @FeignClient(name = "mcsv-usuario", url = "localhost:8090/api/usuario")
 public interface UsuarioClient {
 
     @GetMapping("/search-by-especialidad/{especialidadId}")
-    List<?> findAllUsuariosByEspecialidad(@PathVariable Long especialidadId);
+    List<UsuarioDTO> findAllUsuariosByEspecialidad(@PathVariable Long especialidadId);
 }
