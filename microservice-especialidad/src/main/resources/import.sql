2db06f9d80a08c000daa8e474adbf4fd656a070f
elDone <diego-o.o-@hotmail.com>
2025-06-14 12:50:05 -0500

    sql data test

---
+++ microservice-especialidad/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO especialidad (nombre) VALUES ('General');
+INSERT INTO especialidad (nombre) VALUES ('Odontologia');
+INSERT INTO especialidad (nombre) VALUES ('Gastroenterologia');
+INSERT INTO especialidad (nombre) VALUES ('Pediatria');
