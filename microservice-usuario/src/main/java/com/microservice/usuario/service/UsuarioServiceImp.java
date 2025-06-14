e32cf8f75d63647b643e6f5141e4a8f17f6b41ce
elDone <diego-o.o-@hotmail.com>
2025-06-14 12:49:04 -0500

    fix anotation service

--- microservice-usuario/src/main/java/com/microservice/usuario/service/UsuarioServiceImp.java
+++ microservice-usuario/src/main/java/com/microservice/usuario/service/UsuarioServiceImp.java
@@ -3,11 +3,13 @@ package com.microservice.usuario.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.microservice.usuario.entities.RolUsuario;
 import com.microservice.usuario.entities.Usuario;
 import com.microservice.usuario.persistence.UsuarioRepository;
 
+@Service
 public class UsuarioServiceImp implements IUsuarioService{
 
     @Autowired
